29724a422b10ba59998d09927dbcc819ad358871
Florin Akermann <florin.akermann@gmail.com>
2020-01-13 21:18:03 +0100

    only pull in when both ports are available

--- src/main/java/MapWithExceptionSink.java
+++ src/main/java/MapWithExceptionSink.java
@@ -18,52 +18,44 @@ public class MapWithExceptionSink<I, O0, O1 extends I> extends GraphStage<FanOut
 
     public final Inlet<I> in = Inlet.<I>create("MapWithExceptionSink.in");
     public final Outlet<O0> out = Outlet.<O0>create("MapWithExceptionSink.out");
-    public final Outlet<O1> outAlt = Outlet.<O1>create("MapWithExceptionSink.exceptionOut");
+    public final Outlet<O1> outException = Outlet.<O1>create("MapWithExceptionSink.exceptionOut");
 
-    private final FanOutShape2<I, O0, O1> shape = new FanOutShape2<>(in, out, outAlt);
+    private final FanOutShape2<I, O0, O1> shape = new FanOutShape2<>(in, out, outException);
 
     @Override
     @SuppressWarnings("unchecked")
     public GraphStageLogicWithLogging createLogic(Attributes inheritedAttributes) {
         return new GraphStageLogicWithLogging(shape) {
+
             {
-                setHandler(
-                        in,
-                        new AbstractInHandler() {
-                            @Override
-                            public void onPush() {
-                                I tempIn = grab(in);
-                                O0 result = null;
-                                try {
-                                    result = f.apply(tempIn);
-                                } catch (Exception e) {
-                                    sendExceptionToExceptionSink((O1) tempIn, e);
-                                }
-                                if (result != null) {
-                                    push(out, result);
-                                } else if (!hasBeenPulled(in)) {
-                                    pull(in);
-                                }
+                setHandler(in,
+                        () -> {
+                            I tempIn = grab(in);
+                            O0 result = null;
+                            try {
+                                result = f.apply(tempIn);
+                            } catch (Exception e) {
+                                push(outException, (O1) tempIn);
                             }
-
-                            private void sendExceptionToExceptionSink(O1 tempIn, Exception e) {
-                                if (isAvailable(shape.out1())) {
-                                    push(shape.out1(), tempIn);
-                                    log().info("captured exception: {}, sent to exception sink", e);
-                                } else {
-                                    log().info("captured exception: {}, not sent to exception sink", e);
-                                }
+                            if (result != null) {
+                                push(out, result);
                             }
-                        });
+                        }
+                );
 
-                setHandler(out, () -> pullIfNotPulled());
+                setHandler(out, () -> {
+                    if (isAvailable(outException)) {
+                        pull(in);
+                    }
+                });
 
-                setHandler(outAlt, () -> pullIfNotPulled());
+                setHandler(outException, () -> {
+                    if (isAvailable(out)) {
+                        pull(in);
+                    }
+                });
             }
 
-            private void pullIfNotPulled() {
-                if (!hasBeenPulled(in)) pull(in);
-            }
         };
     }
 
@@ -73,14 +65,11 @@ public class MapWithExceptionSink<I, O0, O1 extends I> extends GraphStage<FanOut
     }
 
     public static <I, O0, M> Flow<I, O0, NotUsed> map(Function<I, O0> fun, Sink<I, M> excSink) {
-        Graph<FlowShape<I, O0>, NotUsed> flowShape = GraphDSL.create(builder -> {
+        return Flow.fromGraph(GraphDSL.create(builder -> {
             final FanOutShape2<I, O0, I> excSinkMap = builder.add(new MapWithExceptionSink<>(fun));
             builder.to(builder.add(excSink)).fromOutlet(excSinkMap.out1());
             return FlowShape.of(excSinkMap.in(), excSinkMap.out0());
-        });
-        return Flow.fromGraph(flowShape);
+        }));
     }
-
-
 }
 
